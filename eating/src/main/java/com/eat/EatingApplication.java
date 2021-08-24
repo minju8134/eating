c98f58a88a299f5accf47a65e194f886564ab174
Owner <Owner@DESKTOP-7DEJEGJ>
2021-08-24 11:49:09 +0900

    Commit

---
+++ eating/src/main/java/com/eat/EatingApplication.java
@@ -0,0 +1,13 @@
+package com.eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatingApplication.class, args);
+	}
+
+}
---
+++ eating/src/test/java/com/eat/EatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
